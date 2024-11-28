775c21398fb40fedb9727df1c5201e83d8ff5fcb
mausalazar404 <a01712976@tec.mx>
2024-11-28 17:20:33 -0600

    Update Mascotas.h

--- Mascotas.h
+++ Mascotas.h
@@ -1,47 +1,96 @@
-#include <string>
-#ifndef MASCOTAS_H
+/*
+ * Proyecto Veterinaria
+ * Mauricio Salazar Nieto
+ * 28/11/2024
+ */
+
+/*
+ * Clase Mascotas contiene los métodos para cambiar y llamar los datos.
+ * Tiene dos clases hijas que representan tipos específicos de mascotas:
+ * Perro y Gato.
+ * Además, maneja una relación con la clase Cuidado, que define 
+ * cuidados asociados a las mascotas.
+ */
+
+
+#include <string>       
+#include "Cuidado.h"    
+#ifndef MASCOTAS_H    
 #define MASCOTAS_H
 
-class Mascotas{
+// Definición de la clase Mascotas
+class Mascotas {
 
 private:
-    std::string nombre;
-    std::string tipo_mascota;
-
+ //Declara las variables de instancia
+    std::string nombre;           
+    std::string tipo_mascota;     
+    Cuidado cuidado;              
 
 public:
-    Mascotas();
-    Mascotas(std::string nom, std::string tipm){
-        nombre = nom;
-        tipo_mascota = tipm;
-    }
+    // Constructores
+    // Constructor por defecto, inicializa los atributos vacíos
+    Mascotas(); 
+    // Constructor que inicializa el nombre y tipo de mascota
+    Mascotas(std::string nom, std::string tip); 
+    // Constructor que inicializa todos los atributos
+    Mascotas(Cuidado cu, std::string nom, std::string tipm);
 
-    std::string get_nombre();
-    std::string get_tipo_mascota();
+    // Métodos getters
+    Cuidado get_cuidado();              
+    std::string get_nombre();           
+    std::string get_tipo_mascota();     
 
-    void set_nombre(std::string);
-    void set_tipo_mascota(std::string);
+    // Métodos setter
+    void set_cuidado(Cuidado);        
+    void set_nombre(std::string);     
+    void set_tipo_mascota(std::string); 
 };
 
-Mascotas::Mascotas(){
+// Implementación del constructor por defecto
+// Inicializa el nombre y tipo de mascota con valores vacíos
+Mascotas::Mascotas() {
     nombre = "";
     tipo_mascota = "";
 }
 
-std::string Mascotas::get_nombre(){
-    return nombre;
-}
-
-std::string Mascotas::get_tipo_mascota(){
-    return tipo_mascota;
-}
-
-void Mascotas::set_nombre(std::string nom){
+// Implementación del constructor 
+Mascotas::Mascotas(std::string nom, std::string tip) {
     nombre = nom;
-}
-
-void Mascotas::set_tipo_mascota(std::string tip){
     tipo_mascota = tip;
 }
 
-#endif
+// Implementación del constructor 
+Mascotas::Mascotas(Cuidado cu, std::string nom, std::string tipm) {
+    cuidado = cu;
+    nombre = nom;
+    tipo_mascota = tipm;
+}
+
+// Métodos getter
+Cuidado Mascotas::get_cuidado() {
+    return cuidado;
+}
+
+std::string Mascotas::get_nombre() {
+    return nombre;
+}
+
+std::string Mascotas::get_tipo_mascota() {
+    return tipo_mascota; 
+}
+
+// Métodos setter
+void Mascotas::set_cuidado(Cuidado cu) {
+    cuidado = cu; 
+}  
+
+void Mascotas::set_nombre(std::string nom) {
+    nombre = nom; 
+}
+
+void Mascotas::set_tipo_mascota(std::string tip) {
+    tipo_mascota = tip; 
+}
+
+#endif 
