8b089fa5ba419a0739295f4719e66a58d62dcc89
mausalazar404 <a01712976@tec.mx>
2024-11-28 17:21:05 -0600

    Update Tienda.h

--- Tienda.h
+++ Tienda.h
@@ -1,36 +1,76 @@
+/*
+ * Proyecto Veterinaria
+ * Mauricio Salazar Nieto
+ * 28/11/2024
+ */
+
+/*
+ * Clase Tienda, maneja grupos de Productos, divididos en
+ * Camas_perros y Cepillo_perros
+ *
+ */
+
+
 #include <string>
 #ifndef TIENDA_H
 #define TIENDA_H
+// Clase que maneja la información de camas para perros
+#include "camas.h" 
+// Clase que maneja la información de cepillos para perros
+#include "cepillo_perros.h" 
 
+// Definición de la clase Tienda
 class Tienda{
-
 private:
+ //Declara las variables de instancia
     std::string nombre;
     std::string ubicacion;
     std::string tamano;
-
+    Cepillo_perros cepillo_perros;
+    Cama_perros cama_perros;
 
 public:
+ // Constructor por defecto
     Tienda();
-    Tienda(std::string nom, std::string ubi, std::string tam){
+ // Constructor con parámetros   
+    Tienda(Cepillo_perros ce, Cama_perros ca, ::string nom, 
+    std::string ubi, std::string tam){
+        cepillo_perros = ce;
+        cama_perros = ca;
         nombre = nom;
         ubicacion = ubi;
         tamano = tam;
     }
 
+    // Métodos getter:
+    Cepillo_perros get_cepillo_perros();
+    Cama_perros get_cama_perros();
     std::string get_nombre();
     std::string get_ubicacion();
     std::string get_tamano();
 
+    // Métodos setter:
+    void set_cepillo_perros(Cepillo_perros);
+    void set_cama_perros(Cama_perros);
     void set_nombre(std::string);
     void set_ubicacion(std::string);
     void set_tamano(std::string);
 };
 
+// Constructor por defecto
 Tienda::Tienda(){
     nombre = "";
     ubicacion = "";
     tamano = "";
+}
+
+// Métodos getter
+Cepillo_perros Tienda::get_cepillo_perros(){
+    return cepillo_perros;
+}
+
+Cama_perros Tienda::get_cama_perros(){
+    return cama_perros;
 }
 
 std::string Tienda::get_nombre(){
@@ -44,6 +84,15 @@ std::string Tienda::get_ubicacion(){
 
 std::string Tienda::get_tamano(){
     return tamano;
+}
+
+// Métodos setter
+void Tienda::set_cepillo_perros(Cepillo_perros ce){
+    cepillo_perros = ce;
+}
+
+void Tienda::set_cama_perros(Cama_perros ca){
+    cama_perros = ca;
 }
 
 void Tienda::set_nombre(std::string nom){
