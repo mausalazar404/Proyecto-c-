b113935307e4aafe7b25b4e30622e3f5ef58a597
mausalazar404 <a01712976@tec.mx>
2024-11-28 17:20:50 -0600

    Update Productos.h

--- Productos.h
+++ Productos.h
@@ -1,37 +1,50 @@
+/*
+ * Proyecto Veterinaria
+ * Mauricio Salazar Nieto
+ * 28/11/2024
+ */
+
+/*
+ * La clase Productos define las propiedades básicas de un producto. 
+ * Incluye atributos como nombre, precio, stock y categoría.
+ * Es la clase base para productos específicos como Cepillo_perros 
+ * y Cama_perros.
+ */
+
 #ifndef PRODUCTOS_H
 #define PRODUCTOS_H
 
-#include "Tienda.h"
-#include <string>
+#include <string> 
 using namespace std;
 
+// Definición de la clase Productos
 class Productos {
 private:
-    Tienda tienda;          
-    string nombre;         
-    float precio;         
-    int stock;            
-    string categoria;    
+    string nombre;      
+    float precio;      
+    int stock;         
+    string categoria;   
 
 public:
-    
-    Productos();
-    Productos(Tienda t, string nom, float pre, int stk, string cat);
+    // Constructores
+    Productos(); // Constructor por defecto
+    // Constructor parametrizado
+    Productos(string nom, float pre, int stk, string cat);
 
-   
-    Tienda get_tienda();
+    // Métodos getters
     string get_nombre();
     float get_precio();
     int get_stock();
     string get_categoria();
 
-    void set_tienda(Tienda t);
+    // Métodos setters
     void set_nombre(string nom);
     void set_precio(float pre);
     void set_stock(int stk);
     void set_categoria(string cat);
 };
 
+// Implementación del constructor por defecto
 Productos::Productos() {
     nombre = "";
     precio = 0.0;
@@ -39,18 +52,15 @@ Productos::Productos() {
     categoria = "";
 }
 
-Productos::Productos(Tienda t, string nom, float pre, int stk, string cat) {
-    tienda = t;
+// Implementación del constructor parametrizado
+Productos::Productos(string nom, float pre, int stk, string cat) {
     nombre = nom;
     precio = pre;
     stock = stk;
     categoria = cat;
 }
 
-Tienda Productos::get_tienda() {
-    return tienda;
-}
-
+// Métodos getters
 string Productos::get_nombre() {
     return nombre;
 }
@@ -67,10 +77,7 @@ string Productos::get_categoria() {
     return categoria;
 }
 
-void Productos::set_tienda(Tienda t) {
-    tienda = t;
-}
-
+// Métodos setters
 void Productos::set_nombre(string nom) {
     nombre = nom;
 }
