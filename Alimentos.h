f68f2188ba183fcfd2b49614b52ae1a4eb84a083
mausalazar404 <a01712976@tec.mx>
2024-11-28 17:19:48 -0600

    Update Alimentos.h

--- Alimentos.h
+++ Alimentos.h
@@ -1,32 +1,52 @@
+/*
+ * Proyecto Veterinaria
+ * Mauricio Salazar Nieto
+ * 28/11/2024
+ */
+
+/*
+ * Clase Alimentos contiene los métodos para cambiar y llamar los datos
+ * Tiene 2 clases hijas que son dos tipos de productos:
+ * Alimento_humedo y Alimento_seco
+ *
+ */
+
 #include <string>
 #ifndef ALIMENTOS_H
 #define ALIMENTOS_H
 
+// Clase base Alimentos
 class Alimentos{
-
 private:
+ //Declara las variables de instancia
     std::string tipo_animal;
     std::string tamano_paquete;
 
 public:
+    // Constructores
     Alimentos();
     Alimentos(std::string tip,std::string tam){
         tipo_animal = tip;
         tamano_paquete = tam;
     }
 
+    // Métodos getter
     std::string get_tipo_animal();
     std::string get_tamano_paquete();
 
+    // Métodos setter
     void set_tipo_animal(std::string);
     void set_tamano_paquete(std::string);
 };
 
+// Implementación del constructor por defecto
+// Inicializa los atributos con valores vacíos
 Alimentos::Alimentos(){
     tipo_animal = "";
     tamano_paquete = "";
 }
 
+//Métodos getter
 std::string Alimentos::get_tipo_animal(){
     return tipo_animal;
 }
@@ -35,6 +55,7 @@ std::string Alimentos::get_tamano_paquete(){
     return tamano_paquete;
 }
 
+//Métodos setter
 void Alimentos::set_tipo_animal(std::string tip){
     tipo_animal = tip;
 }
