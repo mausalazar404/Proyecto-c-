3a53dc0be004f9a8084803c073e8f26debd810b9
mausalazar404 <a01712976@tec.mx>
2024-11-28 17:22:21 -0600

    Update alimento_seco.h

--- alimento_seco.h
+++ alimento_seco.h
@@ -1,42 +1,59 @@
-#include "Alimentos.h"
-#include <string>
+/*
+ * Proyecto Veterinaria
+ * Mauricio Salazar Nieto
+ * 28/11/2024
+ */
+
+/*
+ * La clase Alimento_seco representa un tipo de alimento para mascotas
+ * que hereda de la clase base Alimentos. Incluye atributos específicos
+ * como sabor, marca, edad recomendada y dureza.
+ */
+
+#include "Alimentos.h" 
+#include <string>     
 #ifndef ALIMENTO_SECO_H
 #define ALIMENTO_SECO_H
 
 using namespace std;
 
+// Definición de la clase Alimento_seco
 class Alimento_seco : public Alimentos {
-    private:
-        string sabor;
-        string marca;
-        int edad;
-        string dureza;
+private:
+ //Declara las variables de instancia
+    string sabor;   
+    string marca; 
+    int edad;       
+    string dureza; 
 
-    public:
-        Alimento_seco();
-        
-        Alimento_seco(string tip, string tam, string sab, string mar, int ed, string dur)
-            : Alimentos(tip, tam) {
-            sabor = sab;
-            marca = mar;
-            edad = ed;
-            dureza = dur;
-        }
+public:
+    // Constructores
+    Alimento_seco(); // Constructor por defecto
 
+    // Constructor parametrizado
+    Alimento_seco(string tip, string tam, string sab, 
+        string mar, int ed, string dur)
+        : Alimentos(tip, tam) { // Llama al constructor de la clase base
+        sabor = sab;
+        marca = mar;
+        edad = ed;
+        dureza = dur;
+    }
 
-        string get_sabor();
-        string get_marca();
-        int get_edad();
-        string get_dureza();
+    // Métodos getters
+    string get_sabor();
+    string get_marca();
+    int get_edad();
+    string get_dureza();
 
-
-        void set_sabor(string);
-        void set_marca(string);
-        void set_edad(int);
-        void set_dureza(string);
+    // Métodos setters
+    void set_sabor(string);
+    void set_marca(string);
+    void set_edad(int);
+    void set_dureza(string);
 };
 
-
+// Implementación del constructor por defecto
 Alimento_seco::Alimento_seco() : Alimentos() {
     sabor = "";
     marca = "";
@@ -44,6 +61,7 @@ Alimento_seco::Alimento_seco() : Alimentos() {
     dureza = "";
 }
 
+// Métodos getters
 string Alimento_seco::get_sabor() {
     return sabor;
 }
@@ -60,6 +78,7 @@ string Alimento_seco::get_dureza() {
     return dureza;
 }
 
+// Métodos setters
 void Alimento_seco::set_sabor(string sab) {
     sabor = sab;
 }
